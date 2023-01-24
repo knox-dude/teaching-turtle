5c919d7c1777b84cda8d75a0d566b2ced7ff37a3
Andrew Knox <andrew.knox.r@gmail.com>
2023-01-24 09:58:49 +0100

    Adding simple code to see what happens when downloaded on a local machine.

---
+++ basicExamples.py
@@ -0,0 +1,22 @@
+# ¡Esto siempre debe estar en la parte superior de su código!
+import turtle
+import easygui
+easygui.msgbox("This is a message!", title="simple gui")
+
+# First, create a Turtle object using this command:
+bob = turtle.Turtle()
+# We have now created a Turtle object called bob.
+
+# This is a list of commands that you can use with bob:
+
+"""
+forward(number) -> moves your turtle the way it is facing
+for number amount of pixels
+"""
+bob.forward(100)
+
+"""
+position() -> tells you what position your turtle is at
+"""
+
+turtle.done()
